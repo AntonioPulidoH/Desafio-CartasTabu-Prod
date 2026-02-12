d8949c7ea6335d359cdeb207676e7c0384410fa5
Noelia Barrionuevo <noe.br97@gmail.com>
2026-02-12 21:59:11 +0100

    front. fix redireccion a inicio

--- frontend/src/components/barra-navegacion.tsx
+++ frontend/src/components/barra-navegacion.tsx
@@ -1,17 +1,17 @@
 import { GraduationCap } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 export function BarraNavegacion() {
   const navigate = useNavigate()
   return (
     <nav className="navbar navbar-expand-lg navbar-dark barra-nav fixed-top border-bottom" style={{borderColor: 'var(--color-borde)'}}>
       <div className="container">
-        <a className="navbar-brand d-flex align-items-center gap-2 fs-5 fw-bold text-light" href="#inicio">
+        <Link className="navbar-brand d-flex align-items-center gap-2 fs-5 fw-bold text-light" to='/'>
           <span className="icono-logo d-flex align-items-center justify-content-center">
             {/*Aquí tenemos que poner el logo*/}
             <GraduationCap size={20} />
           </span >
           Tabu-Studio
-        </a>
+        </Link>
 
         <button
           className="navbar-toggler"
