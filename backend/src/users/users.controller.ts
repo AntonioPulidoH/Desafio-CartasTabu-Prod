3c9b8e964909c4122fbeb4da81d63bfc3dfe4cb1
martafronton <martafronton@gmail.com>
2026-02-13 01:38:42 +0100

    feat: registro usuario

---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/user-create.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+constructor(private readonly usersService: UsersService){}
+
+@Post('register') @HttpCode(HttpStatus.OK)
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateUserDto } from "./dto/user-create.dto";
+import * as bcrypt from "bcryptjs";
+
+@Injectable()
+export class UsersService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async findEmail(email: string) {
+        return this.prisma.user.findUnique({
+            where: { email },
+            include: {role: true}
+        });
+    }
+
+    async create(data: CreateUserDto) {
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+
+  return this.prisma.user.create({
+    data: {
+      name: data.name,
+      lastName: data.lastName,
+      email: data.email,
+      password: hashedPassword,
+      educationalCenter: data.educationalCenter,
+      roleId: data.roleId,
+      vocationalFamilyId: data.vocationalFamilyId ?? null,
+    },
+  });
+}
+}
