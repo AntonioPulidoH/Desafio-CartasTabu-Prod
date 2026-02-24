5500e5b19773b011a3b8fa581f2169516c538b9c
Alfredo Carrero Muñiz <alfredoibros@gmail.com>
2026-02-24 13:47:07 +0100

    Componente StatCard

--- frontend/src/components/AdminLayout/AdminLayout.tsx
+++ frontend/src/components/AdminLayout/AdminLayout.tsx
@@ -1,18 +1,32 @@
 import { AdminSidebar } from "../AdminSidebar/AdminSidebar";
+import { StatCard } from "../StatCard/StatCard";
 
 export const AdminLayout = () => {
   return (
     <div className="d-flex" style={{ minHeight: "100vh", width: "100vw" }}>
-      {/* Columna izquierda */}
+      {/* Columna izquierda*/}
       <AdminSidebar />
 
       {/* Columna derecha */}
-      <main
-        className="flex-grow-1 p-4"
-        style={{ backgroundColor: "var(--tabu-gray-bg)" }}
-      >
-        <h2>Panel Administrador</h2>
-        <p>Tarjetas y tabla</p>
+      <main className="flex-grow-1 p-4">
+        <div className="container-fluid max-w-7xl mx-auto">
+          <h2 className="mb-4">Dashboard General</h2>
+
+          {/* StatCards */}
+          <div className="row g-4 mb-5">
+            <div className="col-12 col-md-4">
+              <StatCard title="Usuarios Totales:" value="150" />
+            </div>
+            <div className="col-12 col-md-4">
+              <StatCard title="Tarjetas Generadas:" value="1,200" />
+            </div>
+            <div className="col-12 col-md-4">
+              <StatCard title="Temas Activos:" value="25" />
+            </div>
+          </div>
+
+          {/* Tabla Usuarios */}
+        </div>
       </main>
     </div>
   );
