4c0253285e68f9029f5ce7f0c04472ce07e6e680
Jaime <jaime.ornu@gmail.com>
2026-03-05 23:22:46 +0100

    feat: modificado login de auth controller, añadido login de Google y registro. En auth service añadido el registro y el de Google y modificado el login. Creado archivo google-verify. Añadido datos de Google a env.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) { }
 
     @Post('login')
     @HttpCode(HttpStatus.OK)
     async login(@Body() body: LoginDto) {
-        const user = await this.authService.validateUser(body.email, body.password);
-        return this.authService.login(user);
+        try {
+            const user = await this.authService.validateUser(body.email, body.password);
+            return this.authService.login(user);
+        } catch (error) {
+            console.error('ValidateUser FAILED:', error.message);
+            throw new UnauthorizedException(error.message || 'Invalid credentials');
+        }
+    }
+
+    @Post('google')
+    async googleSignin(@Body('id_token') id_token: string) {
+        return this.authService.googleLogin(id_token);
+    }
+
+    @Post('register')
+    @HttpCode(HttpStatus.OK)
+    async register(@Body() registerDto: RegisterDto) {
+        return this.authService.register(registerDto);
     }
 }
