617d3e8172141dbdd273a0feaef3196c82730509
Jaime <jaime.ornu@gmail.com>
2026-03-05 22:51:04 +0100

    feat: creado dto de registro, cambiado registro de users a auth, en users service modificado el rol de 1 a 3

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -4,10 +4,7 @@ import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-constructor(private readonly usersService: UsersService){}
+  constructor(private readonly usersService: UsersService) { }
 
-@Post('register') @HttpCode(HttpStatus.OK)
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+  
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,63 +1,56 @@
 import { BadRequestException, ConflictException, Injectable, NotFoundException } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 import { CreateUserDto } from "./dto/user-create.dto";
-import * as bcrypt from "bcryptjs";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-    async findEmail(email: string) {
-        return this.prisma.user.findUnique({
-            where: { email },
-            include: {role: true}
-        });
+  async findEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+      include: { role: true }
+    });
+  }
+
+  async create(data: CreateUserDto) {
+    const isValidEmail = (email: string): boolean => {
+      const emailVerified = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      return emailVerified.test(email)
+    }
+    const isValidPassword = (password: string): boolean => {
+      const passwordVerified = /^(?=.*[A-Z])(?=.*\d).{8,}$/
+      return passwordVerified.test(password)
     }
 
-    async create(data: CreateUserDto) {
-  const isValidEmail = (email: string): boolean => {
-    const emailVerified = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    return emailVerified.test(email)
+    if (!data.email || !isValidEmail(data.email)) {
+      throw new BadRequestException('EMAIL_INVALIDO')
+    }
+    if (!data.password || !isValidPassword(data.password)) {
+      throw new BadRequestException('PASSWORD_INVALIDA')
+    }
+    if (!data.name || !data.lastName) {
+      throw new BadRequestException('DATOS_OBLIGATORIOS')
+    }
+
+    const userExists = await this.findEmail(data.email)
+    if (userExists) {
+      throw new ConflictException('EMAIL_YA_REGISTRADO')
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10)
+
+    return this.prisma.user.create({
+      data: {
+        name: data.name,
+        lastName: data.lastName,
+        email: data.email,
+        password: hashedPassword,
+        educationalCenter: data.educationalCenter ?? null,
+        roleId: 3,
+        vocationalFamilyId: data.vocationalFamilyId ?? null,
+      },
+    });
   }
-
-  const isValidPassword = (password: string): boolean => {
-    const passwordVerified = /^(?=.*[A-Z])(?=.*\d).{8,}$/
-    return passwordVerified.test(password)
-  }
-
-  //Validaciones
-  if (!data.email || !isValidEmail(data.email)) {
-    throw new BadRequestException('EMAIL_INVALIDO')
-  }
-
-  if (!data.password || !isValidPassword(data.password)) {
-    throw new BadRequestException('PASSWORD_INVALIDA')
-  }
-
-  if (!data.name || !data.lastName) {
-    throw new BadRequestException('DATOS_OBLIGATORIOS')
-  }
-
-
-  const userExists = await this.findEmail(data.email)
-
-  if (userExists) {
-    throw new ConflictException('EMAIL_YA_REGISTRADO')
-  }
-
-  const hashedPassword = await bcrypt.hash(data.password, 10)
-
-  
-  return this.prisma.user.create({
-    data: {
-      name: data.name,
-      lastName: data.lastName,
-      email: data.email,
-      password: hashedPassword,
-      educationalCenter: data.educationalCenter ?? null,
-      roleId: 1,
-      vocationalFamilyId: data.vocationalFamilyId ?? null,
-    },
-  });
-}
 }
