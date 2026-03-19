3b60f26dc827f683ddfabc1ffd10596a1b7bf622
Noelia Barrionuevo <noe.br97@gmail.com>
2026-03-19 19:36:17 +0100

    feat. front. instalacion paquete iconos bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import "bootstrap-icons/font/bootstrap-icons.css"
 import "./styles/global.css"
 import "../src/components/dashboard/styles/dashboard.css"
 
