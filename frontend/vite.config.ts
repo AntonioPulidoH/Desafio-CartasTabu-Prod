2c8d457dc805341a0f71d180abdc07b2c41b7f34
AntonioPulidoH <antoniophjm@gmail.com>
2026-06-04 19:49:07 +0200

    prod: minor changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,10 +9,17 @@ export default defineConfig({
     allowedHosts: [
       '604eb9e3-6b11-4837-b190.10e38dd8350b.cifpvdg.isard.online'
     ],
-    hmr: {
-      protocol: 'wss',
-      host: '604eb9e3-6b11-4837-b190.10e38dd8350b.cifpvdg.isard.online',
-      clientPort: 443
+    hmr: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      },
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true,
+        changeOrigin: true
+      }
     }
   }
 })
